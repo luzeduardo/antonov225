ccea447ade4dc86085bb58a82c2b5bbedaa0c2c1
Eduardo Luz <luz.eduardo@gmail.com>
2015-05-12 07:51:00 -0300

    model info

--- flyer/flyerapp/models.py
+++ flyer/flyerapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Departures(models.Model):
+	ori = models.CharField(max_length=5)
+	cash = models.DecimalField(max_digits=10, decimal_places=2)
+	pub_date = models.DateTimeField('date published')
+class Landing(models.Model):
+	ori = models.CharField(max_length=5)
+	cash = models.DecimalField(max_digits=10, decimal_places=2)
+	pub_date = models.DateTimeField('date published')
