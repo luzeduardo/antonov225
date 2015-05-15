e9437613e1d690d365105a38cc7325b54dc01809
eduardo.luz <you@example.com>
2015-05-15 18:10:23 -0300

    models anf ignore migrations

--- flyer/flyerapp/models.py
+++ flyer/flyerapp/models.py
@@ -5,22 +5,22 @@ class Place(models.Model):
 	name = models.CharField(max_length=70)
 	iata_code = models.CharField(max_length=5)
 
-class Flight(models.Place):
-	departure = models.ForeignKey(Place)
-	landing = models.ForeignKey(Place)
+class Flight(models.Model):
+	departure = models.ForeignKey(Place, related_name="departure_flight")
+	landing = models.ForeignKey(Place, related_name="landing_flight")
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	departure_date = models.DateTimeField(default=None, null=True) 
 	landing_date = models.DateTimeField(default=None, null=True)
 	pub_date = models.DateTimeField('date published')
-	link = models.CharField()
+	link = models.TextField()
 
-class Schedule(models.Place):
-	departure = models.ForeignKey(Place)
-	landing = models.ForeignKey(Place)
+class Schedule(models.Model):
+	departure = models.ForeignKey(Place, related_name="departure_schedule")
+	landing = models.ForeignKey(Place, related_name="landing_schedule")
 	price = models.DecimalField(max_digits=10, decimal_places=2)
 	departure_date = models.DateTimeField(default=None, null=True) 
 	landing_date = models.DateTimeField(default=None, null=True)
 	days_in_place = models.IntegerField()
-	departure_in_weekeng_only = BooleanField(initial=False)
-	landing_in_weekeng_only = BooleanField(initial=False)
+	departure_in_weekeng_only = models.BooleanField(default=False)
+	landing_in_weekeng_only = models.BooleanField(default=False)
 	pub_date = models.DateTimeField('date published')
