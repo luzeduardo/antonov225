d1d1216414841309d7aed7dab4bdcb7c83ab8cc5
Eduardo Luz <luz.eduardo@gmail.com>
2015-06-12 14:43:32 -0300

    timezone

--- flyer/flyer/settings.py
+++ flyer/flyer/settings.py
@@ -93,9 +93,10 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
