c880a0e4d63a9cd8218a2120d47744a9e8398b8f
Eduardo Luz <luz.eduardo@gmail.com>
2015-06-14 14:20:14 -0300

    adjusting admin preview

--- flyer/flyerapp/admin.py
+++ flyer/flyerapp/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.contrib.admin import helpers
 
 from flyerapp.models import Place, Schedule
 
+def search_flights(modeladmin, request, queryset):
+    ids = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+    for id in ids:
+        schedule = Schedule.objects.filter(id=id).get()
+        departure = Place.objects.filter(id=schedule.departure_id).get()
+        landing = Place.objects.filter(id=schedule.get('landing_id')).get()
+
+    search_flights.short_description = "Search Flights"
+
+class ScheduleAdmin(admin.ModelAdmin):
+    actions = [search_flights]
+
 admin.site.register(Place)
-admin.site.register(Schedule)
+admin.site.register(Schedule, ScheduleAdmin)
