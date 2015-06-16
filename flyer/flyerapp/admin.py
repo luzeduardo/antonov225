00ff0b53f3ee9501ec5b33f1832acab7ff7818c8
Eduardo Luz <luz.eduardo@gmail.com>
2015-06-16 10:59:02 -0300

    refactoring

--- flyer/flyerapp/admin.py
+++ flyer/flyerapp/admin.py
@@ -17,7 +17,10 @@ def search_flights(modeladmin, request, queryset):
     for id in ids:
         schedule = Schedule.objects.filter(id=id).get()
         departure = Place.objects.filter(id=schedule.departure_id).get()
-        landing = Place.objects.filter(id=schedule.landing_id).get()
+        #landing = Place.objects.filter(id=schedule.landing_id).get()
+        landing = {}
+        for lnd in schedule.landing.all():
+            landing[lnd.iata_code] = lnd.name
 
         config_datas = [ [schedule.departure_date,schedule.landing_date] ]
         search(departure, landing, departure.iata_code, landing.iata_code, config_datas, schedule.departure_in_weekend_only, schedule.landing_in_weekend_only, schedule.exactly_days_check, schedule.days_in_place)
@@ -103,97 +106,91 @@ def date_interval(s_year,s_month, s_day, e_year,e_month, e_day):
 
     return datas
 
-def search(departure, landing, origem, destinos, config_datas, ida_durante_semana, volta_durante_semana, exactly_days_check, min_days_in_place):
+def search(departure, landing, config_origem, config_destinos, config_datas, ida_durante_semana, volta_durante_semana, exactly_days_check, min_days_in_place):
     problemas = deque()
     nao_existe = deque()
     google_cheap_price_class = '-c-pb'
     google_processing_price_class = '-j-n'
-    config_origem = {
-        origem:origem
-    }
-    config_destinos = {
-        destinos:destinos
-    }
-    for config_origem in config_origem:
-        for destino in config_destinos.items():
-            for datas in config_datas:
-                try:
-                    #start_time_loop = time.time()
-                    if is_weekend_day(str(datas[0])) and not ida_durante_semana: #ida apenas fds
-                        continue
-                    if is_weekend_day(str(datas[1])) and not volta_durante_semana: #volta apenas fds
-                        continue
-                    if exactly_days_check and not is_valid_min_days_in_place(datas[0], datas[1], min_days_in_place):
-                        continue
-                    config_dia_inicio = str(datas[0])
-                    config_dia_fim = str(datas[1])
-                    #driver = webdriver.Firefox()
-                    driver = webdriver.PhantomJS(service_args=['--ssl-protocol=any'])
-                    driver.set_window_size( 2048, 2048)  # set browser size.
 
-                    url = 'https://www.google.com.br/flights/#search;f=' + config_origem + ';t='+ str(destino[0]) +';d='+config_dia_inicio + ';r=' + config_dia_fim
-                    #print url
-                    driver.get( url )
+    for destino in config_destinos.items():
+        for datas in config_datas:
+            try:
+                #start_time_loop = time.time()
+                if is_weekend_day(str(datas[0])) and not ida_durante_semana: #ida apenas fds
+                    continue
+                if is_weekend_day(str(datas[1])) and not volta_durante_semana: #volta apenas fds
+                    continue
+                if exactly_days_check and not is_valid_min_days_in_place(datas[0], datas[1], min_days_in_place):
+                    continue
+                config_dia_inicio = str(datas[0])
+                config_dia_fim = str(datas[1])
+
+                driver = webdriver.PhantomJS(service_args=['--ssl-protocol=any'])
+                driver.set_window_size( 2048, 2048)  # set browser size.
+
+                url = 'https://www.google.com.br/flights/#search;f=' + config_origem + ';t='+ str(destino[0]) +';d='+config_dia_inicio + ';r=' + config_dia_fim
+                #print url
+                driver.get( url )
+                time.sleep(2)
+                driver.implicitly_wait(2)
+
+                core = driver.find_element_by_css_selector('#root')
+                class_name = core.get_attribute("class")
+                class_splited = class_name.split('-',1)
+                final_class = '.' + class_splited[0] + google_cheap_price_class
+                wait_class = '.' + class_splited[0] + google_processing_price_class
+
+                #Testa se elemento de processamento sumiu e processegue com o script
+                element_existe = True
+                teste_processando = 0
+                while element_existe:
+                    try:
+                        teste_processando += 1
+                        resultado = driver.find_element_by_css_selector(wait_class)
+                        time.sleep(1)
+                        driver.implicitly_wait(1)
+                        if teste_processando == 1:
+                            element_existe = False
+                    except NoSuchElementException, e:
+                        element_existe = False
+
+                try:
                     time.sleep(2)
                     driver.implicitly_wait(2)
+                    resultado = driver.find_element_by_css_selector(final_class)
+                    # data hora consulta, origem, valor, data pesquisada ida, data pesquisada volta, destino, url acesso
+                    valor_exibicao = resultado.text
+                    valor_processado = valor_exibicao.split("R$")
+                    valor_processado = valor_processado[1]
+                    valor_processado = re.sub('[^0-9]+', '', valor_processado)
 
-                    core = driver.find_element_by_css_selector('#root')
-                    class_name = core.get_attribute("class")
-                    class_splited = class_name.split('-',1)
-                    final_class = '.' + class_splited[0] + google_cheap_price_class
-                    wait_class = '.' + class_splited[0] + google_processing_price_class
+                    fly = Flight()
+                    fly.departure = departure
+                    fly.landing = landing
+                    fly.price = valor_processado
+                    fly.departure_date = config_dia_inicio
+                    fly.landing_date = config_dia_fim
+                    fly.link = url
+                    fly.save()
 
-                    #Testa se elemento de processamento sumiu e processegue com o script
-                    element_existe = True
-                    teste_processando = 0
-                    while element_existe:
-                        try:
-                            teste_processando += 1
-                            resultado = driver.find_element_by_css_selector(wait_class)
-                            time.sleep(1)
-                            driver.implicitly_wait(1)
-                            if teste_processando == 1:
-                                element_existe = False
-                        except NoSuchElementException, e:
-                            element_existe = False
+                    #print "Valor" + "\t"+ valor_exibicao + "\t" + valor_processado + "\t" + config_dia_inicio + "\t" + config_dia_fim + "\t" + str(config_origem) + "\t" + str(destino[1]) + "\t" + str(destino[0]) + "\t" + url  + "\t" + datetime.now().strftime("%d/%m/%Y") + "\t" + datetime.now().strftime("%H:%M")
 
-                    try:
-                        time.sleep(2)
-                        driver.implicitly_wait(2)
-                        resultado = driver.find_element_by_css_selector(final_class)
-                        # data hora consulta, origem, valor, data pesquisada ida, data pesquisada volta, destino, url acesso
-                        valor_exibicao = resultado.text
-                        valor_processado = valor_exibicao.split("R$")
-                        valor_processado = valor_processado[1]
-                        valor_processado = re.sub('[^0-9]+', '', valor_processado)
-
-                        fly = Flight()
-                        fly.departure = departure
-                        fly.landing = landing
-                        fly.price = valor_processado
-                        fly.departure_date = config_dia_inicio
-                        fly.landing_date = config_dia_fim
-                        fly.link = url
-                        fly.save()
-
-                        #print "Valor" + "\t"+ valor_exibicao + "\t" + valor_processado + "\t" + config_dia_inicio + "\t" + config_dia_fim + "\t" + str(config_origem) + "\t" + str(destino[1]) + "\t" + str(destino[0]) + "\t" + url  + "\t" + datetime.now().strftime("%d/%m/%Y") + "\t" + datetime.now().strftime("%H:%M")
-
-                        driver.quit()
-                    except NoSuchElementException, e:
-                        notfound_class = '.' + class_splited[0] + '-Pb-e'
-                        resultado = driver.find_element_by_css_selector(notfound_class)
-                        for ne in nao_existe:
-                            if str(ne) == str(destino[1]):
-                                problemas.append('Ignorar destino: ' + str(destino[1]))
-                        nao_existe.append(str(destino[1]))
-                        driver.quit()
-                    except Exception, e:
-                        problemas.append('Problema ao retornar valor de: ' + str(destino[1]) +"\t" + url)
-                        driver.quit()
-                    #print("--- %s seconds ---" % (time.time() - start_time_loop))
-                except Exception, e:
-                    problemas.append('Problema ao retornar elemento principal: ' + str(destino[1]) +"\t" + url)
                     driver.quit()
+                except NoSuchElementException, e:
+                    notfound_class = '.' + class_splited[0] + '-Pb-e'
+                    resultado = driver.find_element_by_css_selector(notfound_class)
+                    for ne in nao_existe:
+                        if str(ne) == str(destino[1]):
+                            problemas.append('Ignorar destino: ' + str(destino[1]))
+                    nao_existe.append(str(destino[1]))
+                    driver.quit()
+                except Exception, e:
+                    problemas.append('Problema ao retornar valor de: ' + str(destino[1]) +"\t" + url)
+                    driver.quit()
+                #print("--- %s seconds ---" % (time.time() - start_time_loop))
+            except Exception, e:
+                problemas.append('Problema ao retornar elemento principal: ' + str(destino[1]) +"\t" + url)
+                driver.quit()
 
 
 class FlightAdmin(admin.ModelAdmin):
