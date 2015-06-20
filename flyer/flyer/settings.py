760c07ce433cd10ce6297453ad11d66206514921
Eduardo Luz <luz.eduardo@gmail.com>
2015-06-20 14:39:15 -0300

    setup

--- flyer/flyer/settings.py
+++ flyer/flyer/settings.py
@@ -166,6 +166,7 @@ LANGUAGES = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
