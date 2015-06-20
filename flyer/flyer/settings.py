693d70033cef4e1d2812ede23e071015f23e8177
Eduardo Luz <luz.eduardo@gmail.com>
2015-06-20 13:48:33 -0300

    angular setup

--- flyer/flyer/settings.py
+++ flyer/flyer/settings.py
@@ -43,7 +43,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'bootstrap3',
-    # 'django_admin_bootstrapped',
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
