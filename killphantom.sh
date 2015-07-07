b17102696fd02384a3a9e994964ec7d672406ef0
Eduardo Luz <luz.eduardo@gmail.com>
2015-07-07 17:28:51 -0300

    actions

--- killphantom.sh
+++ killphantom.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-find /proc -maxdepth 1 -type d -mmin +3 -exec basename {} \; \
+find /proc -maxdepth 1 -user luzeduardo -type d -mmin +3 -exec basename {} \; \
 | xargs ps | grep phantomjs | awk '{ print $1 }' | sudo xargs kill
 
 #*/2 * * * * /home/luzeduardo/killphantom.sh
